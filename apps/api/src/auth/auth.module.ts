94ab136ede703291022c5e979f6b40f75189bab8
mustaphalimar <tapham079@gmail.com>
2024-10-23 22:14:15 +0100

    sign-in nextjs server action

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserService } from '@/user/user.service';
 import { PrismaService } from '@/prisma/prisma.service';
+import { LocalStrategy } from './strategies/local.startegy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, UserService, PrismaService],
+  providers: [AuthService, UserService, PrismaService, LocalStrategy],
 })
 export class AuthModule {}
--- apps/web/actions/auth.ts
+++ apps/web/actions/auth.ts
@@ -1,6 +1,6 @@
 "use server";
 import { BACKEND_URL } from "@/lib/constants";
-import { FormState, SignupFormSchema } from "@/lib/types";
+import { FormState, LoginFormSchema, SignupFormSchema } from "@/lib/types";
 import { redirect } from "next/navigation";
 
 export async function signUp(
@@ -32,6 +32,40 @@ export async function signUp(
   } else {
     return {
       message: res.status == 409 ? "Email already exists." : res.statusText,
+    };
+  }
+}
+
+export async function signIn(
+  state: FormState,
+  formData: FormData
+): Promise<FormState> {
+  const validatedFields = LoginFormSchema.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      error: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+  const res = await fetch(`${BACKEND_URL}/auth/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(validatedFields.data),
+  });
+
+  if (res.ok) {
+    const result = await res.json();
+    // create the session for authenticated user
+    console.log(result);
+  } else {
+    return {
+      message: res.status === 401 ? "Invalid credentials" : res.statusText,
     };
   }
 }
